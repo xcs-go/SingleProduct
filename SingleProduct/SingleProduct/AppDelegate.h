56e941697a0798953dd9bbe2e00ad00a48048ca2
xcs <13162927913@163.com>
2016-06-06 21:02:11 +0800

    init

---
+++ SingleProduct/SingleProduct/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SingleProduct
+//
+//  Created by 谢朝森 on 16/6/6.
+//  Copyright (c) 2016年 xcs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SingleProduct/SingleProduct/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SingleProduct
+//
+//  Created by 谢朝森 on 16/6/6.
+//  Copyright (c) 2016年 xcs. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
